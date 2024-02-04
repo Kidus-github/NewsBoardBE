5c528e771e74965adaec524d7a93ea4deeedce07
Kidus-github <kidus.muluwork@gmail.com>
2024-02-04 04:23:40 +0300

    created TagCollection and TagService

---
+++ NewsBoardTrail/Controllers/TagController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewsBoardBE.Modals;
+using NewsBoardBE.Services;
+
+namespace NewsBoardBE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        private INewsBoardServices<Tag> _tagCollection;
+
+        public TagController(INewsBoardServices<Tag> tag)
+        {
+            _tagCollection = tag;
+
+        }
+        // GET: api/<TagController>
+        [HttpGet]
+        public List<Tag> Get()
+        {
+            return _tagCollection.Get();
+        }
+
+        // GET api/<TagController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Tag> Get(string id)
+        {
+            var tag = _tagCollection.GetById(id);
+            if (tag == null)
+            {
+                return NotFound($"Tag with Id = {id} not found");
+            }
+            return tag;
+        }
+
+        // POST api/<TagController>
+        [HttpPost]
+        public ActionResult<Tag> Post([FromBody] Tag value)
+        {
+            _tagCollection.Create(value);
+            return CreatedAtAction(nameof(Get), new { id = value.TagId }, value);
+        }
+
+        // PUT api/<TagController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(string id, [FromBody] Tag value)
+        {
+            var existing = _tagCollection.GetById(id);
+            if (existing == null) { return NotFound($"Tag with Id = {id} not found"); }
+            _tagCollection.Update(id, value);
+            return Ok($"Tag with id = {id} Updated");
+        }
+
+        // DELETE api/<TagController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            var existing = _tagCollection.GetById(id);
+            if (existing == null) { return NotFound($"Tag with Id = {id} not found"); }
+            _tagCollection.Delete(id);
+            return Ok($"Tag with Id = {id} deleted");
+        }
+    }
+}
---
+++ NewsBoardTrail/Services/TagService.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Driver;
+using NewsBoardBE.Modals;
+
+namespace NewsBoardBE.Services
+{
+    public class TagService : INewsBoardServices<Tag>
+    {
+        private readonly IMongoCollection<Tag> _tagCollection;
+
+        public TagService(INewsBoardDatabaseSettings settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+
+            _tagCollection = database.GetCollection<Tag>(settings.TagCollectionName);
+        }
+
+        public Tag Create(Tag entity)
+        {
+            _tagCollection.InsertOne(entity);
+            return entity;
+        }
+
+        public void Delete(string id)
+        {
+            _tagCollection.DeleteOne(tag => tag.TagId == id);
+        }
+
+        public List<Tag> Get()
+        {
+            return _tagCollection.Find(tag => true).ToList();
+        }
+
+        public Tag GetById(string id)
+        {
+            return _tagCollection.Find(tag => tag.TagId == id).FirstOrDefault();
+        }
+
+        public void Update(string id, Tag entity)
+        {
+            _tagCollection.ReplaceOne(tag => tag.TagId == id, entity);
+        }
+
+    }
+}
