c64796797fc4c3180bd4ff0b69b150db8a21e922
Kidus-github <kidus.muluwork@gmail.com>
2024-02-04 04:32:07 +0300

    created trendingController and TrendingService

---
+++ NewsBoardTrail/Controllers/TrendingController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewsBoardBE.Services;
+
+namespace NewsBoardBE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrendingController : ControllerBase
+    {
+        private INewsBoardServices<Trending> _trendingCollection;
+
+        public TrendingController(INewsBoardServices<Trending> trending)
+        {
+            _trendingCollection = trending;
+
+        }
+        // GET: api/<TrendingController>
+        [HttpGet]
+        public List<Trending> Get()
+        {
+            return _trendingCollection.Get();
+        }
+
+        // GET api/<TrendingController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Trending> Get(string id)
+        {
+            var trending = _trendingCollection.GetById(id);
+            if (trending == null)
+            {
+                return NotFound($"Trending with Id = {id} not found");
+            }
+            return trending;
+        }
+
+        // POST api/<TrendingController>
+        [HttpPost]
+        public ActionResult<Trending> Post([FromBody] Trending value)
+        {
+            _trendingCollection.Create(value);
+            return CreatedAtAction(nameof(Get), new { id = value.TrendingId }, value);
+        }
+
+        // PUT api/<TrendingController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(string id, [FromBody] Trending value)
+        {
+            var existing = _trendingCollection.GetById(id);
+            if (existing == null) { return NotFound($"Trending with Id = {id} not found"); }
+            _trendingCollection.Update(id, value);
+            return Ok($"Trending with id = {id} Updated");
+        }
+
+        // DELETE api/<TrendingController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            var existing = _trendingCollection.GetById(id);
+            if (existing == null) { return NotFound($"Trending with Id = {id} not found"); }
+            _trendingCollection.Delete(id);
+            return Ok($"Trending with Id = {id} deleted");
+        }
+    }
+}
+}
---
+++ NewsBoardTrail/Services/TrendingService.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Driver;
+using NewsBoardBE.Modals;
+
+namespace NewsBoardBE.Services
+{
+    public class TrendingService : INewsBoardServices<Trending>
+    {
+        private readonly IMongoCollection<Trending> _trendingCollection;
+
+        public TrendingService(INewsBoardDatabaseSettings settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+
+            _trendingCollection = database.GetCollection<Trending>(settings.TrendingCollectionName);
+        }
+
+        public Trending Create(Trending entity)
+        {
+            _trendingCollection.InsertOne(entity);
+            return entity;
+        }
+
+        public void Delete(string id)
+        {
+            _trendingCollection.DeleteOne(trending => trending.TrendingID == id);
+        }
+
+        public List<Trending> Get()
+        {
+            return _trendingCollection.Find(trending => true).ToList();
+        }
+
+        public Trending GetById(string id)
+        {
+            return _trendingCollection.Find(trending => trending.TrendingID == id).FirstOrDefault();
+        }
+
+        public void Update(string id, Trending entity)
+        {
+            _trendingCollection.ReplaceOne(trending => trending.TrendingID == id, entity);
+        }
+
+    }
+}
